3416e4a8dd4d8daf753cc2ccfcb7aa97b8d91729
ninggf <windywany@163.com>
2024-06-21 13:06:48 +0800

    theme

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,13 +2,13 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 import { persist } from '@/utils'
-
+const sysInfo = uni.getSystemInfoSync()
 export const useAppStore = defineStore(
   'app',
   () => {
     const app = ref({
-      language: 'en',
-      theme: 'auto'
+      language: uni.getLocale(),
+      theme: sysInfo.theme || 'light'
     })
 
     return {
