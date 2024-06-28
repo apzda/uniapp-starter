97ca323e00b5501c94d98439f821b3ae0de7423d
ninggf <windywany@gmail.com>
2024-06-28 18:33:13 +0800

    加解密工具

---
+++ src/utils/crypto.ts
@@ -0,0 +1,28 @@
+import CryptoJS from 'crypto-js'
+
+const opts = {
+  iv: CryptoJS.enc.Utf8.parse(import.meta.env.VITE_ENC_IV)
+}
+
+const alg = import.meta.env.VITE_ENC_ALG || 'aes' == 'aes' ? CryptoJS.AES : CryptoJS.DES
+const key = import.meta.env.VITE_ENC_KEY
+
+export function decrypt(text: string): string {
+  return alg.decrypt(text, key, opts).toString(CryptoJS.enc.Utf8)
+}
+
+export function decryptObj<T>(text: string): T | null {
+  const decrypted = decrypt(text)
+  if (decrypted) {
+    return JSON.parse(decrypted) as T
+  }
+  return null
+}
+
+export function encrypt(text: string): string {
+  return alg.encrypt(text, key, opts).toString()
+}
+
+export function encryptObj(obj: Record<string, any>): string {
+  return encrypt(JSON.stringify(obj))
+}
